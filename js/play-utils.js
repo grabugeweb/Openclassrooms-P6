faca6f4faa519bc982b603f534eabeabd613c62e
remimichel <remi.michel38@gmail.com>
2018-03-04 12:53:23 +0100

    revue du 04/03/2018

--- js/play-utils.js
+++ js/play-utils.js
@@ -38,7 +38,7 @@ function changePlayerAtEachTurn() {
 }
 
 
-function playerMove(element, player, forbiddenIds) {
+function playerMove(element, player, forbiddenIds, holes) {
   const coordonateEnd = {
     ligne: $(element).data('ligne'),
     colonne: $(element).data('colonne'),
@@ -56,7 +56,7 @@ function playerMove(element, player, forbiddenIds) {
   //const allowedCoordonates = getAllowedMoves();
   //const isAuthorized = allowedCoordonates.find((c) => coordonateEnd === c);
   //player= idCurrentPlayer.concat(player.id); MARCHE PAS ?????
-  if (isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCasesUtilisees)) {
+  if (isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCasesUtilisees, holes)) {
     console.log('function isMoveAllowed ON');
     $('#' + player.id).html('');
     player.id = coordonateEnd.id;
@@ -67,26 +67,29 @@ function playerMove(element, player, forbiddenIds) {
     player.case_utilisee = nbreCasesUtilisees;
     console.log('case utilisée = ' + player.case_utilisee);
     console.log('l' + player.ligne, 'c' + player.colonne);
-  };
+  }
+  ;
 
-  if ((nbreCasesUtilisees >= valeurDes) && (isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCasesUtilisees))) {
+  if ((nbreCasesUtilisees >= valeurDes) && (isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCasesUtilisees, holes))) {
     $('#nbreCase').text('');
     $('#resultat').text('');
     player.case_utilisee = 0;
     changePlayerAtEachTurn(player1, player2);
     currentPlayer = getCurrentPlayer(player1, player2);
     console.log(currentPlayer);
-  };
+  }
+  ;
 
-  if (!(isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCasesUtilisees))) {
+  if (!(isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCasesUtilisees, holes))) {
     alert('Mauvais déplacement , Re-commence !!');
-  };
+  }
+  ;
 
   /* gestion pass trhough
   - déctecter si click produit un nbre decases utilisées > à 2 (donc un click)
   - récupérer les ids des cellules traversées
   - les pousser dans un tableau puis ocder dans isallowed
-  
+
   if (Math.abs(player.ligne-coordonateEnd.ligne)!= 1) {
     if (player.ligne != coordonateEnd.ligne) {
 
@@ -95,7 +98,7 @@ function playerMove(element, player, forbiddenIds) {
 
 }
 
-function isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCasesUtilisees) {
+function isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCasesUtilisees, holes) {
   const {
     ligne,
     colonne,
@@ -105,15 +108,41 @@ function isMoveAllowed(player, coordonateEnd, forbiddenIds, valeurDes, nbreCases
   //Move diagonally
   isAllowedConditions = isAllowedConditions.concat(!(player.ligne !== ligne && player.colonne !== colonne));
   //Move is not on a hole
-  forbiddenIds = forbiddenIds.concat(forbiddenIds.splice(numberOfHoles, 6)); // QUESTION Obtenir la mêm chose avec un map() ?? + ne fonctionnait pas car je n'avais pas mis forbiddenIds.concat
-  //A CORRIGER forbiddenIds=forbiddenIds.map(t => t.id!==armes.id);
-  isAllowedConditions = isAllowedConditions.concat(!forbiddenIds.find(e => e === parseInt(id, 10)));
-  //Move has to be inside value dé 
+  const holeIds = forbiddenIds.filter((e, idx) => idx < numberOfHoles);
+  isAllowedConditions = isAllowedConditions.concat(!holeIds.find(holeId => holeId === parseInt(id, 10)));
+
+  //Vérifier si il y a un trou sur la trajectoire
+  isAllowedConditions = isAllowedConditions.concat(!hasHoleInDeplacement({x: player.colonne, y: player.ligne}, {x: coordonateEnd.colonne, y: coordonateEnd.ligne}, holes));
+
+
+  //Move has to be inside value dé
   isAllowedConditions = isAllowedConditions.concat(nbreCasesUtilisees <= valeurDes);
   // return true and false : when reduce meets false --> player stop
   return isAllowedConditions.reduce((acc, curr) => (!acc) ? acc : curr, true);
 }
 
+
+// Object, Object, Array<Hole> -> Boolean
+function hasHoleInDeplacement(startCoordonate, endCoordonate, holes) {
+  if (isHorizontalMove(startCoordonate, endCoordonate)) { //attribut colonne des trous dans l'interval de déplacement
+    const sortedCordonnates = [startCoordonate, endCoordonate].sort((a, b) => a.x > b.x);
+    return holes
+      .filter((hole) => hole.ligneHole === startCoordonate.y) //On filtre les trous qui sont sur la meme ligne que le joueur --> Cela retourne un nouveau tableau
+      .filter((hole) => hole.colonneHole > sortedCordonnates[0].x && hole.colonneHole < sortedCordonnates[1].x) //On filtre à nouveau sur le résultat du premier filtre les trous qui sont dans la trajectoire du déplacement
+      .length > 0;
+  }
+  // On refait la même manipulation dans l'autre sens car déplacement vertical
+  const sortedCordonnates = [startCoordonate, endCoordonate].sort((a, b) => a.y > b.y);
+  return holes
+    .filter((hole) => hole.colonneHole === startCoordonate.x) //On filtre les trous qui sont sur la meme colonne que le joueur
+    .filter((hole) => hole.ligneHole > sortedCordonnates[0].y && hole.ligneHole < sortedCordonnates[1].y)
+    .length > 0;
+}
+
+
+function isHorizontalMove(c1, c2) {
+  return c1.x !== c2.x && c1.y === c2.y;
+}
 
 
 /*function checkMoves(player, ligneFin, colonneFin, diagId, forbiddenIds) {
