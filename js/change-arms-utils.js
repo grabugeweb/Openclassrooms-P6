e879fcf09844c12ff80bc751c0c6562b4c66ace2
remimichel <remi.michel38@gmail.com>
2018-03-11 12:22:30 +0100

    code review du 11/03

--- js/change-arms-utils.js
+++ js/change-arms-utils.js
@@ -1,31 +1,51 @@
 function ifWeaponIsOnCase(player) {
-    let weaponOnCase = [];
-    if (armes.find((c) => c.id == player.id)) { // QUESTION c = n'importe quel objet du tab armes ?
-        weaponOnCase = weaponOnCase.concat(armes.find((c) => c.id == player.id))
-        changeArmes(player, weaponOnCase);
-    };
+  const armeFound = findArmeById(player.id);
+  if (armeFound) {
+    changeArmes(player, armeFound);
+  }
 }
 
 function changeArmes(player, newWeaponPlayer) {
-    // stock Weapon Player
-    const stockOldPlayerWeapon = player.arme;
-    // refresh player weapon
-    player.arme = newWeaponPlayer[0].name;
-    newWeaponPlayer[0].id = player.id; // QUESTION on dirait que ça concat direct dans le tab armes
-    newWeaponPlayer[0].ligne = player.ligne;
-    newWeaponPlayer[0].colonne = player.colonne;
-    console.log('new weapon',newWeaponPlayer);
-    weaponCase = [];
-    // refresh oldweapon
-    let oldWeaponPlayer = [];
-    oldWeaponPlayer = oldWeaponPlayer.concat(armes.find((c) => c.name == stockOldPlayerWeapon));
-    oldWeaponPlayer[0].ligne = player.ligne;
-    oldWeaponPlayer[0].colonne = player.colonne;
-    oldWeaponPlayer[0].id = player.id;
-    const img = $(oldWeaponPlayer[0].avatar);
-    img.appendTo($('td[id=' + oldWeaponPlayer[0].id + ']'));
-    console.log('old weapon',oldWeaponPlayer);
-    oldWeaponPlayer=[];
-    console.log(armes);
-    console.log(player);
+  // stock Weapon Player
+  const stockOldPlayerWeapon = player.arme;
+  // refresh player weapon
+  player.arme = newWeaponPlayer.name;
+  newWeaponPlayer.id = player.id;
+  newWeaponPlayer.ligne = player.ligne;
+  newWeaponPlayer.colonne = player.colonne;
+  updateArmes(newWeaponPlayer);
+  // refresh oldweapon
+  const oldWeaponPlayer = findArmeByName(stockOldPlayerWeapon);
+  oldWeaponPlayer.ligne = player.ligne;
+  oldWeaponPlayer.colonne = player.colonne;
+  oldWeaponPlayer.id = player.id;
+  updateArmes(oldWeaponPlayer);
+  updateArmesDOM(oldWeaponPlayer);
+}
+
+function findArmeByName(armeName) {
+  return Object.assign({}, armes.find((arme) => arme.name === armeName)); // Retourne une copie de l'objet arme et non pas l'arme directement
+}
+
+function findArmeById(armeId) {
+  //Renommage des ids des armes et des players en caseId
+  const armeFound = armes.find((arme) => arme.id == armeId);
+  if(armeFound){
+    return Object.assign({}, armeFound); // Retourne une copie de l'objet arme et non pas l'arme directement
+  }
+  return null;
+}
+
+function updateArmesDOM(oldWeaponPlayer) {
+  const img = $(oldWeaponPlayer.avatar);
+  img.appendTo($('td[id=' + oldWeaponPlayer.id + ']'));
+}
+
+function updateArmes(arme) {
+  armes = armes.map((a) => {
+    if (a.name === arme.name) {
+      return arme;
+    }
+    return a;
+  })
 }
